637f16042d2c10bef1f1032c111aecfc4be65421
rosh_01 <mohammedajeez2004@gmail.com>
2025-02-09 12:42:54 +0530

    Update firebase.jsx

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -10,7 +10,7 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBczrppB5HKS41ojF2OAcAQbLLvYOLK4RA",
+  apiKey: "",
   authDomain: "laborsloom.firebaseapp.com",
   projectId: "laborsloom",
   storageBucket: "laborsloom.firebasestorage.app",
